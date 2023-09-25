6ba428f2f6669be0f24a2530a646faac7ea5cd65
work <jeremy.barnsley@esynergy.co.uk>
2023-09-25 13:01:25 +0100

    Temp debug

--- RPA.MIT.Notification.Function/Notification.cs
+++ RPA.MIT.Notification.Function/Notification.cs
@@ -86,7 +86,7 @@ namespace RPA.MIT.Notification
         }
 
         [Function("CheckConfig")]
-        public async Task CheckConfig(
+        public void CheckConfig(
             [TimerTrigger("0 */2 * * * *")] TimerInfo myTimer)
         {
             Console.WriteLine("CheckConfig function executed - console");
@@ -94,6 +94,13 @@ namespace RPA.MIT.Notification
             var queueConnString = _configuration.GetSection("QueueConnectionString").Value;
             _logger.LogInformation("CheckConfig queueConnectionString=" + (string.IsNullOrEmpty(queueConnString) ? "null" : queueConnString.Substring(0,40)));
             Console.WriteLine("CheckConfig queueConnectionString - console=" + (string.IsNullOrEmpty(queueConnString) ? "null" : queueConnString.Substring(0,40)));
+        }
+
+        [Function("CheckConfigStatic")]
+        public static void CheckConfigStatic(
+            [TimerTrigger("0 */2 * * * *")] TimerInfo myTimer)
+        {
+            Console.WriteLine("CheckConfigStatic function executed - console");
         }
 
         /*
