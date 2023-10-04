0b42f101a47417b28c809b550eaf7dc7e17b7b5f
work <jeremy.barnsley@esynergy.co.uk>
2023-10-04 16:44:32 +0100

    yaml typo

--- RPA.MIT.Notification.Function/Program.cs
+++ RPA.MIT.Notification.Function/Program.cs
@@ -18,10 +18,17 @@ var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
-        Console.WriteLine("Startup.ConfigureServices() called");
+        Console.WriteLine("Startup.ConfigureServices()1 called");
         var serviceProvider = services.BuildServiceProvider();
 
         var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+        var qn1 = configuration.GetSection("ServiceBusEventQueueName").Value;
+        Console.WriteLine("Startup ServiceBus eventQueueName1 = " + qn1);
+        var qn2 = configuration.GetSection("ServiceBusNotificationQueueName").Value;
+        Console.WriteLine("Startup ServiceBus notificationQueueName1 = " + qn2);
+        var mi = configuration.GetSection("ServiceBusEventConnectionString:fullyQualifiedNamespace").Value;
+        Console.WriteLine("Startup ServiceBus endpoint1 = " + mi);
 
         services.AddScoped<ISenderFactory, SenderFactory>();
         services.AddSingleton<INotificationClient>(_ => new NotificationClient(configuration.GetSection("NotifyApiKey").Value));
@@ -30,13 +37,13 @@ var host = new HostBuilder()
         {
             // Constructors are slightly different dpending if using Managed Identity or SAS connection string
             var managedIdentityNamespace = configuration.GetSection("ServiceBusEventConnectionString:fullyQualifiedNamespace").Value;
-            Console.WriteLine("Startup ServiceBus endpoint = " + managedIdentityNamespace);
+            Console.WriteLine("Startup ServiceBus endpoint2 = " + managedIdentityNamespace);
             var connectionString = configuration.GetSection("ServiceBusEventConnectionString").Value;
             var serviceBusClient = string.IsNullOrEmpty(managedIdentityNamespace)
                 ? new ServiceBusClient(connectionString)
                 : new ServiceBusClient(managedIdentityNamespace, new DefaultAzureCredential()); // ManagedIdentityCredential());
             var queueName = configuration.GetSection("ServiceBusEventQueueName").Value;
-            Console.WriteLine("Startup ServiceBus queueName = " + queueName);
+            Console.WriteLine("Startup ServiceBus queueName2 = " + queueName);
             return new EventQueueService(serviceBusClient, queueName, new SenderFactory());
         });
         services.AddSingleton<INotificationTable>(_ =>
